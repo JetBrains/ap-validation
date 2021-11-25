0db4c31d07f975b8e9bb1d458a461a177f9f9fa6
Anastasia Ivanova <anastasia.ivanova@jetbrains.com>
2021-11-25 14:44:16 +0100

    AP-2937 Unify versions in all clients libraries
    GitOrigin-RevId: b8f787b7656db588f48577537f755638b16bc6f4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,27 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val isCommonProject = findProperty("is.common.project") == "true"
+
 plugins {
-    kotlin("jvm") version "1.4.21"
+    kotlin("jvm")
     java
-    id("maven-publish")
 }
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies") }
+    if (!isCommonProject) {
+        maven { url = uri("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies") }
+    }
 }
-
-group = "org.jetbrains.intellij.deps"
-version  = "0.0.${findProperty("teamcity")
-    ?.takeIf { it is Map<*, *> }
-    ?.let { (it as Map<*, *>)["build.number"] }
-    ?: "x-SNAPSHOT"}"
-val artifactID = "ap-validation"
-
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.21")
     implementation("org.jetbrains:annotations:20.1.0")
-    implementation("com.jetbrains.fus.reporting:model:60")
+    if (!isCommonProject) {
+        implementation("com.jetbrains.fus.reporting:model:60")
+    } else {
+        implementation(project(":projects:model"))
+    }
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.4.21")
     testImplementation("com.google.code.gson:gson:2.8.6")
     testImplementation("junit:junit:4.12")
@@ -43,38 +42,4 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     withSourcesJar()
     withJavadocJar()
-}
-
-val publicationName = "MyPublication"
-publishing {
-    publications.invoke {
-        create<MavenPublication>(publicationName) {
-            from(components["java"])
-            groupId = project.group.toString()
-            artifactId = artifactID
-            version = project.version.toString()
-
-            pom {
-                name.set(rootProject.name)
-                description.set("Library for validating statistics events before sending them to the server")
-                licenses {
-                    licenses {
-                        license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                    }
-                }
-            }
-        }
-    }
-    repositories {
-        maven {
-            url = uri("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
-            credentials {
-                username = findProperty("spaceClientId") as? String
-                password = findProperty("spaceSecret") as? String
-            }
-        }
-    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
 rootProject.name = "ap-validation"
+
+pluginManagement {
+    val isCommonProject:String? by settings
+
+    if (isCommonProject != "true") {
+        plugins {
+            kotlin("jvm") version "1.4.21"
+        }
+    }
+}
